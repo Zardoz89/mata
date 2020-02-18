54b7d1939d6c639ba54aebb9a2986f6f95a9a40f
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-02-18 15:21:19 +0100

    Renombrado dosbox.conf

--- div.sh
+++ div.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #if ! [ -x "$(command -v dosbox-x)" ]; then
-  dosbox --conf dosbox.conf ./div.bat &
+  dosbox --conf dosbox-mio.conf ./div.bat &
 #else
 #  dosbox-x --conf dosbox-x.conf ./div.bat &
 #fi

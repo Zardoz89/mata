5c6d2744851ed5fed4fe68646f49cca51c824f8e
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-02-23 17:19:33 +0100

    Agregado tiles y probando herramienta para generar tilemaps

---
+++ mata/map/tiles/Tyr.tsx
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Tyr" tilewidth="24" tileheight="28" tilecount="63" columns="3">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="24" height="28" source="png/bdw01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="24" height="28" source="png/bdw01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="24" height="28" source="png/bdw02.png"/>
+ </tile>
+ <tile id="3">
+  <image width="24" height="28" source="png/bdw03.png"/>
+ </tile>
+ <tile id="4">
+  <image width="24" height="28" source="png/bdw04.png"/>
+ </tile>
+ <tile id="5">
+  <image width="24" height="28" source="png/bdw05.png"/>
+ </tile>
+ <tile id="6">
+  <image width="24" height="28" source="png/bdw06.png"/>
+ </tile>
+ <tile id="7">
+  <image width="24" height="28" source="png/bdw07.png"/>
+ </tile>
+ <tile id="8">
+  <image width="24" height="28" source="png/bdw08.png"/>
+ </tile>
+ <tile id="9">
+  <image width="24" height="28" source="png/bdw09.png"/>
+ </tile>
+ <tile id="10">
+  <image width="24" height="28" source="png/bdw10.png"/>
+ </tile>
+ <tile id="11">
+  <image width="24" height="28" source="png/bdw11.png"/>
+ </tile>
+ <tile id="12">
+  <image width="24" height="28" source="png/bdw12.png"/>
+ </tile>
+ <tile id="13">
+  <image width="24" height="28" source="png/brown00.png"/>
+ </tile>
+ <tile id="14">
+  <image width="24" height="28" source="png/brown00.png"/>
+ </tile>
+ <tile id="15">
+  <image width="24" height="28" source="png/brownr00.png"/>
+ </tile>
+ <tile id="16">
+  <image width="24" height="28" source="png/dw-00.png"/>
+ </tile>
+ <tile id="17">
+  <image width="24" height="28" source="png/dw-01.png"/>
+ </tile>
+ <tile id="18">
+  <image width="24" height="28" source="png/dw-02.png"/>
+ </tile>
+ <tile id="19">
+  <image width="24" height="28" source="png/dw-03.png"/>
+ </tile>
+ <tile id="20">
+  <image width="24" height="28" source="png/dw-04.png"/>
+ </tile>
+ <tile id="21">
+  <image width="24" height="28" source="png/water00.png"/>
+ </tile>
+ <tile id="22">
+  <image width="24" height="28" source="png/coast00.png"/>
+ </tile>
+ <tile id="23">
+  <image width="24" height="28" source="png/coast01.png"/>
+ </tile>
+ <tile id="24">
+  <image width="24" height="28" source="png/coast02.png"/>
+ </tile>
+ <tile id="25">
+  <image width="24" height="28" source="png/coast03.png"/>
+ </tile>
+ <tile id="26">
+  <image width="24" height="28" source="png/coast04.png"/>
+ </tile>
+ <tile id="27">
+  <image width="24" height="28" source="png/coast05.png"/>
+ </tile>
+ <tile id="28">
+  <image width="24" height="28" source="png/coast06.png"/>
+ </tile>
+ <tile id="29">
+  <image width="24" height="28" source="png/coast07.png"/>
+ </tile>
+ <tile id="30">
+  <image width="24" height="28" source="png/coast08.png"/>
+ </tile>
+ <tile id="31">
+  <image width="24" height="28" source="png/coast09.png"/>
+ </tile>
+ <tile id="32">
+  <image width="24" height="28" source="png/coast10.png"/>
+ </tile>
+ <tile id="33">
+  <image width="24" height="28" source="png/coast11.png"/>
+ </tile>
+ <tile id="34">
+  <image width="24" height="28" source="png/gbrown01.png"/>
+ </tile>
+ <tile id="35">
+  <image width="24" height="28" source="png/gbrown02.png"/>
+ </tile>
+ <tile id="36">
+  <image width="24" height="28" source="png/gbrown03.png"/>
+ </tile>
+ <tile id="37">
+  <image width="24" height="28" source="png/gbrown04.png"/>
+ </tile>
+ <tile id="38">
+  <image width="24" height="28" source="png/gbrown05.png"/>
+ </tile>
+ <tile id="39">
+  <image width="24" height="28" source="png/gbrown06.png"/>
+ </tile>
+ <tile id="40">
+  <image width="24" height="28" source="png/gbrown07.png"/>
+ </tile>
+ <tile id="41">
+  <image width="24" height="28" source="png/gbrown08.png"/>
+ </tile>
+ <tile id="42">
+  <image width="24" height="28" source="png/gbrown09.png"/>
+ </tile>
+ <tile id="43">
+  <image width="24" height="28" source="png/gbrown10.png"/>
+ </tile>
+ <tile id="44">
+  <image width="24" height="28" source="png/gbrown11.png"/>
+ </tile>
+ <tile id="45">
+  <image width="24" height="28" source="png/gbrown12.png"/>
+ </tile>
+ <tile id="46">
+  <image width="24" height="28" source="png/metal00.png"/>
+ </tile>
+ <tile id="47">
+  <image width="24" height="28" source="png/mebr00.png"/>
+ </tile>
+ <tile id="48">
+  <image width="24" height="28" source="png/mebr01.png"/>
+ </tile>
+ <tile id="49">
+  <image width="24" height="28" source="png/mebr02.png"/>
+ </tile>
+ <tile id="50">
+  <image width="24" height="28" source="png/green-00.png"/>
+ </tile>
+ <tile id="51">
+  <image width="24" height="28" source="png/green-02.png"/>
+ </tile>
+ <tile id="52">
+  <image width="24" height="28" source="png/gfor01.png"/>
+ </tile>
+ <tile id="53">
+  <image width="24" height="28" source="png/gfor02.png"/>
+ </tile>
+ <tile id="54">
+  <image width="24" height="28" source="png/gfor03.png"/>
+ </tile>
+ <tile id="55">
+  <image width="24" height="28" source="png/gfor04.png"/>
+ </tile>
+ <tile id="56">
+  <image width="24" height="28" source="png/gfor05.png"/>
+ </tile>
+ <tile id="57">
+  <image width="24" height="28" source="png/gfor06.png"/>
+ </tile>
+ <tile id="58">
+  <image width="24" height="28" source="png/gfor07.png"/>
+ </tile>
+ <tile id="59">
+  <image width="24" height="28" source="png/gfor08.png"/>
+ </tile>
+ <tile id="60">
+  <image width="24" height="28" source="png/gfor09.png"/>
+ </tile>
+ <tile id="61">
+  <image width="24" height="28" source="png/gfor10.png"/>
+ </tile>
+ <tile id="62">
+  <image width="24" height="28" source="png/gfor11.png"/>
+ </tile>
+</tileset>

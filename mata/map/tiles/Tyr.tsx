72c40206ac1432e59d7b57f2f6a0cb4af94008db
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-03-02 23:01:36 +0100

    Configurando Terrains para Tiled

--- mata/map/tiles/Tyr.tsx
+++ mata/map/tiles/Tyr.tsx
@@ -1,46 +1,55 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.2" name="Tyr" tilewidth="24" tileheight="28" tilecount="112" columns="3">
  <grid orientation="orthogonal" width="1" height="1"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="13"/>
+  <terrain name="Brown" tile="16"/>
+  <terrain name="Green" tile="50"/>
+  <terrain name="Wood" tile="56"/>
+  <terrain name="Water" tile="21"/>
+  <terrain name="Light Grey Tech" tile="46"/>
+  <terrain name="Light Gret Tech Wall" tile="91"/>
+ </terraintypes>
  <tile id="0">
   <image width="24" height="28" source="png/novalid.png"/>
  </tile>
- <tile id="1">
+ <tile id="1" terrain="1,1,1,0">
   <image width="24" height="28" source="png/bdw01.png"/>
  </tile>
- <tile id="2">
+ <tile id="2" terrain="1,0,1,0">
   <image width="24" height="28" source="png/bdw02.png"/>
  </tile>
- <tile id="3">
+ <tile id="3" terrain="1,0,1,1">
   <image width="24" height="28" source="png/bdw03.png"/>
  </tile>
- <tile id="4">
+ <tile id="4" terrain="1,1,0,0">
   <image width="24" height="28" source="png/bdw04.png"/>
  </tile>
- <tile id="5">
+ <tile id="5" terrain="0,0,1,1">
   <image width="24" height="28" source="png/bdw05.png"/>
  </tile>
- <tile id="6">
+ <tile id="6" terrain="1,1,0,1">
   <image width="24" height="28" source="png/bdw06.png"/>
  </tile>
- <tile id="7">
+ <tile id="7" terrain="0,1,0,1">
   <image width="24" height="28" source="png/bdw07.png"/>
  </tile>
- <tile id="8">
+ <tile id="8" terrain="0,1,1,1">
   <image width="24" height="28" source="png/bdw08.png"/>
  </tile>
- <tile id="9">
+ <tile id="9" terrain="1,0,0,0">
   <image width="24" height="28" source="png/bdw09.png"/>
  </tile>
- <tile id="10">
+ <tile id="10" terrain="0,0,1,0">
   <image width="24" height="28" source="png/bdw10.png"/>
  </tile>
- <tile id="11">
+ <tile id="11" terrain="0,1,0,0">
   <image width="24" height="28" source="png/bdw11.png"/>
  </tile>
- <tile id="12">
+ <tile id="12" terrain="0,0,0,1">
   <image width="24" height="28" source="png/bdw12.png"/>
  </tile>
- <tile id="13">
+ <tile id="13" terrain="0,0,0,0">
   <image width="24" height="28" source="png/brown00.png"/>
  </tile>
  <tile id="14">
@@ -49,7 +58,7 @@
  <tile id="15">
   <image width="24" height="28" source="png/brownr00.png"/>
  </tile>
- <tile id="16">
+ <tile id="16" terrain="1,1,1,1">
   <image width="24" height="28" source="png/dw-00.png"/>
  </tile>
  <tile id="17">
@@ -64,130 +73,130 @@
  <tile id="20">
   <image width="24" height="28" source="png/dw-04.png"/>
  </tile>
- <tile id="21">
+ <tile id="21" terrain="4,4,4,4">
   <image width="24" height="28" source="png/water00.png"/>
  </tile>
- <tile id="22">
+ <tile id="22" terrain="4,4,4,0">
   <image width="24" height="28" source="png/coast00.png"/>
  </tile>
- <tile id="23">
+ <tile id="23" terrain="4,0,4,0">
   <image width="24" height="28" source="png/coast01.png"/>
  </tile>
- <tile id="24">
+ <tile id="24" terrain="4,0,4,4">
   <image width="24" height="28" source="png/coast02.png"/>
  </tile>
- <tile id="25">
+ <tile id="25" terrain="4,4,0,0">
   <image width="24" height="28" source="png/coast03.png"/>
  </tile>
- <tile id="26">
+ <tile id="26" terrain="0,0,4,4">
   <image width="24" height="28" source="png/coast04.png"/>
  </tile>
- <tile id="27">
+ <tile id="27" terrain="4,4,0,4">
   <image width="24" height="28" source="png/coast05.png"/>
  </tile>
- <tile id="28">
+ <tile id="28" terrain="0,4,0,4">
   <image width="24" height="28" source="png/coast06.png"/>
  </tile>
- <tile id="29">
+ <tile id="29" terrain="0,4,4,4">
   <image width="24" height="28" source="png/coast07.png"/>
  </tile>
- <tile id="30">
+ <tile id="30" terrain="4,0,0,0">
   <image width="24" height="28" source="png/coast08.png"/>
  </tile>
- <tile id="31">
+ <tile id="31" terrain="0,0,4,0">
   <image width="24" height="28" source="png/coast09.png"/>
  </tile>
- <tile id="32">
+ <tile id="32" terrain="0,4,0,0">
   <image width="24" height="28" source="png/coast10.png"/>
  </tile>
- <tile id="33">
+ <tile id="33" terrain="0,0,0,4">
   <image width="24" height="28" source="png/coast11.png"/>
  </tile>
- <tile id="34">
+ <tile id="34" terrain="2,2,2,0">
   <image width="24" height="28" source="png/gbrown01.png"/>
  </tile>
- <tile id="35">
+ <tile id="35" terrain="2,0,2,0">
   <image width="24" height="28" source="png/gbrown02.png"/>
  </tile>
- <tile id="36">
+ <tile id="36" terrain="2,0,2,2">
   <image width="24" height="28" source="png/gbrown03.png"/>
  </tile>
- <tile id="37">
+ <tile id="37" terrain="2,2,0,0">
   <image width="24" height="28" source="png/gbrown04.png"/>
  </tile>
- <tile id="38">
+ <tile id="38" terrain="0,0,2,2">
   <image width="24" height="28" source="png/gbrown05.png"/>
  </tile>
- <tile id="39">
+ <tile id="39" terrain="2,2,0,2">
   <image width="24" height="28" source="png/gbrown06.png"/>
  </tile>
- <tile id="40">
+ <tile id="40" terrain="0,2,0,2">
   <image width="24" height="28" source="png/gbrown07.png"/>
  </tile>
- <tile id="41">
+ <tile id="41" terrain="0,2,2,2">
   <image width="24" height="28" source="png/gbrown08.png"/>
  </tile>
- <tile id="42">
+ <tile id="42" terrain="2,0,0,0">
   <image width="24" height="28" source="png/gbrown09.png"/>
  </tile>
- <tile id="43">
+ <tile id="43" terrain="0,0,2,0">
   <image width="24" height="28" source="png/gbrown10.png"/>
  </tile>
- <tile id="44">
+ <tile id="44" terrain="0,2,0,0">
   <image width="24" height="28" source="png/gbrown11.png"/>
  </tile>
- <tile id="45">
+ <tile id="45" terrain="0,0,0,2">
   <image width="24" height="28" source="png/gbrown12.png"/>
  </tile>
- <tile id="46">
+ <tile id="46" terrain="5,5,5,5">
   <image width="24" height="28" source="png/metal00.png"/>
  </tile>
- <tile id="47">
+ <tile id="47" terrain="0,0,5,5">
   <image width="24" height="28" source="png/mebr00.png"/>
  </tile>
- <tile id="48">
+ <tile id="48" terrain="0,0,0,5">
   <image width="24" height="28" source="png/mebr01.png"/>
  </tile>
- <tile id="49">
+ <tile id="49" terrain="0,0,5,0">
   <image width="24" height="28" source="png/mebr02.png"/>
  </tile>
- <tile id="50">
+ <tile id="50" terrain="2,2,2,2">
   <image width="24" height="28" source="png/green-00.png"/>
  </tile>
  <tile id="51">
   <image width="24" height="28" source="png/green-02.png"/>
  </tile>
- <tile id="52">
+ <tile id="52" terrain="2,2,2,3">
   <image width="24" height="28" source="png/gfor01.png"/>
  </tile>
- <tile id="53">
+ <tile id="53" terrain="2,3,2,3">
   <image width="24" height="28" source="png/gfor02.png"/>
  </tile>
- <tile id="54">
+ <tile id="54" terrain="2,3,2,2">
   <image width="24" height="28" source="png/gfor03.png"/>
  </tile>
- <tile id="55">
+ <tile id="55" terrain="2,2,3,3">
   <image width="24" height="28" source="png/gfor04.png"/>
  </tile>
- <tile id="56">
+ <tile id="56" terrain="3,3,3,3">
   <image width="24" height="28" source="png/gfor05.png"/>
  </tile>
- <tile id="57">
+ <tile id="57" terrain="3,3,,">
   <image width="24" height="28" source="png/gfor06.png"/>
  </tile>
- <tile id="58">
+ <tile id="58" terrain="2,2,3,2">
   <image width="24" height="28" source="png/gfor07.png"/>
  </tile>
- <tile id="59">
+ <tile id="59" terrain="3,2,3,2">
   <image width="24" height="28" source="png/gfor08.png"/>
  </tile>
- <tile id="60">
+ <tile id="60" terrain="3,2,2,2">
   <image width="24" height="28" source="png/gfor09.png"/>
  </tile>
- <tile id="61">
+ <tile id="61" terrain="3,3,,3">
   <image width="24" height="28" source="png/gfor10.png"/>
  </tile>
- <tile id="62">
+ <tile id="62" terrain="3,3,2,2">
   <image width="24" height="28" source="png/gfor11.png"/>
  </tile>
  <tile id="63">
@@ -244,94 +253,94 @@
  <tile id="80">
   <image width="24" height="28" source="png/aroad-15.png"/>
  </tile>
- <tile id="81">
+ <tile id="81" terrain="5,0,5,0">
   <image width="24" height="28" source="png/mebr03.png"/>
  </tile>
- <tile id="82">
+ <tile id="82" terrain="0,5,0,5">
   <image width="24" height="28" source="png/mebr04.png"/>
  </tile>
- <tile id="83">
+ <tile id="83" terrain="5,0,6,0">
   <image width="24" height="28" source="png/mebr05.png"/>
  </tile>
- <tile id="84">
+ <tile id="84" terrain="0,5,0,6">
   <image width="24" height="28" source="png/mebr06.png"/>
  </tile>
- <tile id="85">
+ <tile id="85" terrain="5,5,6,6">
   <image width="24" height="28" source="png/metal01.png"/>
  </tile>
- <tile id="86">
+ <tile id="86" terrain="6,0,0,0">
   <image width="24" height="28" source="png/mebr07.png"/>
  </tile>
- <tile id="87">
+ <tile id="87" terrain="6,6,0,0">
   <image width="24" height="28" source="png/mebr08.png"/>
  </tile>
- <tile id="88">
+ <tile id="88" terrain="0,6,0,0">
   <image width="24" height="28" source="png/mebr09.png"/>
  </tile>
- <tile id="89">
+ <tile id="89" terrain="0,,0,">
   <image width="24" height="28" source="png/mebr10.png"/>
  </tile>
- <tile id="90">
+ <tile id="90" terrain=",0,,0">
   <image width="24" height="28" source="png/mebr11.png"/>
  </tile>
- <tile id="91">
+ <tile id="91" terrain="6,6,6,6">
   <image width="24" height="28" source="png/metal02.png"/>
  </tile>
  <tile id="92">
   <image width="24" height="28" source="png/metal03.png"/>
  </tile>
- <tile id="93">
+ <tile id="93" terrain="4,4,4,">
   <image width="24" height="28" source="png/m-01-02.png"/>
  </tile>
- <tile id="94">
+ <tile id="94" terrain="4,,4,">
   <image width="24" height="28" source="png/m-01-03.png"/>
  </tile>
- <tile id="95">
+ <tile id="95" terrain="4,,4,6">
   <image width="24" height="28" source="png/m-01-04.png"/>
  </tile>
- <tile id="96">
+ <tile id="96" terrain="4,6,4,6">
   <image width="24" height="28" source="png/m-01-05.png"/>
  </tile>
- <tile id="97">
+ <tile id="97" terrain="4,6,4,4">
   <image width="24" height="28" source="png/m-01-06.png"/>
  </tile>
- <tile id="98">
+ <tile id="98" terrain="4,4,5,5">
   <image width="24" height="28" source="png/m-01-07.png"/>
  </tile>
- <tile id="99">
+ <tile id="99" terrain="4,4,5,5">
   <image width="24" height="28" source="png/m-02-02.png"/>
  </tile>
- <tile id="100">
+ <tile id="100" terrain="4,4,5,4">
   <image width="24" height="28" source="png/m-03-02.png"/>
  </tile>
- <tile id="101">
+ <tile id="101" terrain="5,4,5,4">
   <image width="24" height="28" source="png/m-03-03.png"/>
  </tile>
- <tile id="102">
+ <tile id="102" terrain="5,4,6,4">
   <image width="24" height="28" source="png/m-03-04.png"/>
  </tile>
- <tile id="103">
+ <tile id="103" terrain="6,4,6,4">
   <image width="24" height="28" source="png/m-03-05.png"/>
  </tile>
- <tile id="104">
+ <tile id="104" terrain="6,4,4,4">
   <image width="24" height="28" source="png/m-03-06.png"/>
  </tile>
- <tile id="105">
+ <tile id="105" terrain="4,5,0,5">
   <image width="24" height="28" source="png/wmc00.png"/>
  </tile>
- <tile id="106">
+ <tile id="106" terrain="6,6,4,4">
   <image width="24" height="28" source="png/m-06-04.png"/>
  </tile>
- <tile id="107">
+ <tile id="107" terrain="0,,0,">
   <image width="24" height="28" source="png/de0101.png"/>
  </tile>
- <tile id="108">
+ <tile id="108" terrain="0,,0,">
   <image width="24" height="28" source="png/de0102.png"/>
  </tile>
- <tile id="109">
+ <tile id="109" terrain=",0,,0">
   <image width="24" height="28" source="png/de0201.png"/>
  </tile>
- <tile id="110">
+ <tile id="110" terrain=",0,,0">
   <image width="24" height="28" source="png/de0202.png"/>
  </tile>
  <tile id="111">
